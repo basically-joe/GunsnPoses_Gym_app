bc58af92146b145ecb41b9a98184c3115ee4b12b
Joe McColl <joe4kool@gmail.com>
2018-12-14 10:00:06 +0000

    created sql for initial tables with many to many relationship

---
+++ db/gym.sql
@@ -0,0 +1,18 @@
+CREATE TABLE events (
+id SERIAL8 PRIMARY KEY,
+title VARCHAR(255),
+time_slot VARCHAR(255),
+type VARCHAR(255)
+);
+
+CREATE TABLE clients (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  age INT4,
+);
+
+CREATE TABLE bookings (
+  id SERIAL8 PRIMARY KEY,
+  events_id INT4 REFERENCES events(id),
+  bookings_id INT4 REFERENCES events(id),
+);
