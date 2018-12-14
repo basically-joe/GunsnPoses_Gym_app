cb32db84720e24f82538755208083b2716e8f6bc
Joe McColl <joe4kool@gmail.com>
2018-12-14 10:06:23 +0000

    added tables to gym.sql, runs in IDE and check tables are live in Postico

--- db/gym.sql
+++ db/gym.sql
@@ -1,3 +1,7 @@
+DROP TABLE bookings;
+DROP TABLE events;
+DROP TABLE clients;
+
 CREATE TABLE events (
 id SERIAL8 PRIMARY KEY,
 title VARCHAR(255),
@@ -8,11 +12,11 @@ type VARCHAR(255)
 CREATE TABLE clients (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
-  age INT4,
+  age INT4
 );
 
 CREATE TABLE bookings (
   id SERIAL8 PRIMARY KEY,
   events_id INT4 REFERENCES events(id),
-  bookings_id INT4 REFERENCES events(id),
+  bookings_id INT4 REFERENCES events(id)
 );
