58fb6416ca78ccc2548d651c378dc22c463dd8da
Joe McColl <joe4kool@gmail.com>
2018-12-15 18:30:37 +0000

    added post method to controller for eventsd and tested adding new class to events class. working in sinatra and in new entry in DB

--- controllers/events_controller.rb
+++ controllers/events_controller.rb
@@ -13,6 +13,11 @@ get '/events/new' do
   erb ( :"events/new")
 end
 
+post '/events' do
+  Event.new(params).save
+  redirect to '/events'
+end
+
 get "/events/:id" do
   @events = Event.find(params['id'].to_i)
   erb ( :"events/show")
