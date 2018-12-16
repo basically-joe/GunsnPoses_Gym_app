80e00e7ebd2d0e43ae9be50703094d8885af5e30
Joe McColl <joe4kool@gmail.com>
2018-12-16 14:01:38 +0000

    added show route and edit route to bookings controller and working in browser using sinatra

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,6 +1,23 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative( '../models/event' )
-require_relative( '../models/client' )
 require_relative( '../models/booking' )
+require_relative( '../models/client' )
+require_relative( '../models/event' )
 also_reload( '../models/*' )
+
+get '/bookings' do
+  @bookings = Booking.all
+  @clients = Client.all
+  @events = Event.all
+  erb ( :"bookings/index" )
+end
+
+get "/bookings/:id" do
+  @bookings = Booking.find(params['id'].to_i)
+  erb ( :"bookings/show")
+end
+
+get '/bookings/:id/edit' do
+  @bookings = Booking.find(params['id'].to_i)
+  erb ( :"bookings/edit")
+end
