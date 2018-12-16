ae06b779a0731bdca30937f8ca38caf4ca7c76c4
Joe McColl <joe4kool@gmail.com>
2018-12-16 16:21:40 +0000

    removed edit.erb and show.erb for bookings

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -7,17 +7,10 @@ also_reload( '../models/*' )
 
 get '/bookings' do
   @bookings = Booking.all
-  @clients = Client.all
-  @events = Event.all
   erb ( :"bookings/index" )
 end
 
 get "/bookings/:id" do
   @bookings = Booking.find(params['id'].to_i)
   erb ( :"bookings/show")
-end
-
-get '/bookings/:id/edit' do
-  @bookings = Booking.find(params['id'].to_i)
-  erb ( :"bookings/edit")
 end
