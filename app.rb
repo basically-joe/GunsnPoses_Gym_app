b86a836cb820c229f280a9352d99ad9ae162ebae
Joe McColl <joe4kool@gmail.com>
2018-12-16 10:56:15 +0000

    added clients index page and route, working in browser via sinatra

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/events_controller')
+require_relative('controllers/clients_controller')
 
 get '/' do
   erb( :index )
--- controllers/clients_controller.rb
+++ controllers/clients_controller.rb
@@ -2,3 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/client' )
 also_reload( '../models/*' )
+
+get "/clients" do
+  @clients = Event.all()
+  erb ( :"clients/index")
+end
