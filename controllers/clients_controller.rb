031d2b947bff080fd12d51e854f4cd623a31efff
Joe McColl <joe4kool@gmail.com>
2018-12-16 11:15:16 +0000

    added list of clients to Clients index and tested as working in browser via sinatra

--- controllers/clients_controller.rb
+++ controllers/clients_controller.rb
@@ -7,3 +7,18 @@ get "/clients" do
   @clients = Client.all()
   erb ( :"clients/index")
 end
+
+get '/clients/new' do
+  @clients = Client.all()
+  erb ( :"clients/new")
+end
+
+post '/clients' do
+  Client.new(params).save
+  redirect to '/clients'
+end
+
+get "/clients/:id" do
+  @clients = Client.find(params['id'].to_i)
+  erb ( :"clients/show")
+end
