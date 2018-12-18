a567461b6b8c9a8c6adcbdcfcd8bc19f45ebc7d7
Joe McColl <joe4kool@gmail.com>
2018-12-18 11:51:55 +0000

    added quick booking to home page and updated app.rb to make this work tested in browser as working via sinatra

--- app.rb
+++ app.rb
@@ -5,5 +5,7 @@ require_relative('controllers/clients_controller')
 require_relative('controllers/bookings_controller')
 
 get '/' do
+  @events = Event.all
+  @clients = Client.all
   erb( :index )
 end
